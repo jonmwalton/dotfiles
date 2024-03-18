3a6c388825f58f462b59836bb3e7f6535629f55d
Jon Walton <jonwalton@manjaro.localdomain>
2024-03-18 15:44:54 +0000

    pywal and rofi

--- install.sh
+++ install.sh
@@ -1,1 +1,19 @@
+sudo dnf copr enable alebastr/sway-extras
+
+swww
+hyprland
 stow
+default-fonts
+lxappearance
+ImageMagick
+python3-pip
+swayidle
+wlogout
+codium
+gtk-murrine-engine
+pulseaudio
+fontawesome-fonts
+
+
+pip3 install pywal
+wal -i ~/dotfiles/wallpaper/
