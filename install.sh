6a674385be660597353ef878e3069746c396da76
Jon Walton <jwalton@redhat.com>
2024-04-16 16:38:13 +0100

    waybar changes

--- install.sh
+++ install.sh
@@ -26,7 +26,8 @@ LOG="install-$(date +%d-%H%M%S).log"
 sudo dnf copr enable -y solopasha/hyprland
 sudo dnf copr enable -y erikreider/SwayNotificationCenter
 sudo dnf copr enable -y tofik/nwg-shell
-sudo dnf copr enable eddsalkield/swaylock-effects
+#sudo dnf copr enable eddsalkield/swaylock-effects
+#sudo dnf copr enable tiritor/waybar #includes cava support
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
 
@@ -36,7 +37,7 @@ packages=(
     blueman
     SwayNotificationCenter
     hyprland
-    waybar
+    waybar-git
     #hyprlock
     #hypridle
     swayidle
